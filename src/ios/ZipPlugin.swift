dc1b808e093e4d0c805e81125acdef22553d6ee0
dineshkokare56 <81946841+dineshkokare56@users.noreply.github.com>
2021-07-02 16:46:25 +0530

    added.

--- src/ios/ZipPlugin.swift
+++ src/ios/ZipPlugin.swift
@@ -49,6 +49,7 @@ func getSourceDictionary(_ sourceString: String?) -> [AnyHashable : Any]? {
 }
 
 
+
 // jsevent method.
 func jsEvent(_ event: String?, _ data: String?) {
     var eventStrig = "cordova.fireDocumentEvent('\(event ?? "")'"
@@ -59,6 +60,7 @@ func jsEvent(_ event: String?, _ data: String?) {
     eventStrig = eventStrig + ");"
    commandDelegate.evalJs(eventStrig)
 }
+
 
 // dic method.
 func dictionary(toJSONString toCast: [AnyHashable : Any]?) -> String? {
