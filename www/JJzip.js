6ae093b63ebee7629b4031dd65317d1d719be5e8
Antonio Vargas Garcia <antonio.vargas@outlook.com>
2021-12-18 00:14:04 -0800

    iOS Improvements
    * Support creating ZIP file in iOS from folder and from list of files
    * Run plugin logic asynchronously

--- www/JJzip.js
+++ www/JJzip.js
@@ -4,40 +4,23 @@
  * See a full copy of license in the root folder of the project
  */
 
-var argscheck       = require('cordova/argscheck'),
-    exec            = require('cordova/exec');
+var argscheck = require('cordova/argscheck'),
+    exec = require('cordova/exec');
 
 function JJzip() { }
 
-/**
- *
- * @param {String} Path to the file or directory to zip
- * @param {String} Object with options of the action like
- * {
- *      target  : "Path/to/place/result/file"
- *      , name  : if set, put the result this name, otherwise put the source file name  
- * }
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-JJzip.prototype.zip = function(file, options, successCallback, errorCallback) {
-    argscheck.checkArgs('sOFF', 'JJzip.zip', arguments);
-    exec(successCallback, errorCallback, "JJzip", "zip", [file, options]);
+JJzip.prototype.zipFiles = function (file, options, successCallback, errorCallback) {
+    argscheck.checkArgs('ASFF', 'JJzip.zip', arguments);
+    exec(successCallback, errorCallback, "JJzip", "zipFiles", [file, options]);
 };
 
-/**
- *
- * @param {String} Path to the zip file
- * @param {String} Object with options of the action like
- * {
- *      target  : "Path/to/place/result/file"
- *      , name  : if set, put the result this name, otherwise put the source file name  
- * }
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-JJzip.prototype.unzip = function(file, options, successCallback, errorCallback) {
-    argscheck.checkArgs('sOFF', 'JJzip.unzip', arguments);
+JJzip.prototype.zipFolder = function (file, options, successCallback, errorCallback) {
+    argscheck.checkArgs('SSFF', 'JJzip.zip', arguments);
+    exec(successCallback, errorCallback, "JJzip", "zipFolder", [file, options]);
+};
+
+JJzip.prototype.unzip = function (file, options, successCallback, errorCallback) {
+    argscheck.checkArgs('SSFF', 'JJzip.unzip', arguments);
     exec(successCallback, errorCallback, "JJzip", "unzip", [file, options]);
 };
 
